38a94caa7f84b85bad9acac9436227512d3344cd
jessedesimone <desimone.neuro@gmail.com>
2021-10-01 16:38:17 -0400

    update

---
+++ sm_lin_regress.py
@@ -0,0 +1,146 @@
+#!/usr/local/bin/python3.9
+
+'''
+Code derived from Kaushik Katari (https://towardsdatascience.com/simple-linear-regression-model-using-python-machine-learning-eab7924d18b4)
+'''
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+import statsmodels.api as sm
+from sklearn.metrics import r2_score
+
+# Read the given CSV file, and view some sample records
+inpath = '/Users/jessedesimone/DeSimone_Github/python_stats/Datasets/'
+df = pd.read_csv(inpath + 'Company_data.csv')
+df.info()
+df.describe()
+
+# Visualize the data wrt to Sales
+sns.pairplot(df, x_vars=['TV', 'Radio','Newspaper'],
+             y_vars='Sales', size=4, aspect=1, kind='scatter')
+plt.show()
+
+# Visualize the data using heatmap
+sns.heatmap(df.corr(), cmap="YlGnBu", annot = True)
+plt.show()
+
+# Create X and y
+X = df['TV']
+y = df['Sales']
+
+# # Splitting the varaibles as training and testing
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    train_size = 0.7,
+                                                    test_size = 0.3,
+                                                    random_state = 100)
+X_train
+y_train
+
+# Building and training the model
+# Adding a constant to get an intercept
+X_train_sm = sm.add_constant(X_train)
+
+# Fitting the resgression line using 'OLS'
+lr = sm.OLS(y_train, X_train_sm).fit()
+
+# Printing the parameters
+lr.params
+
+# Performing a summary to list out all the different parameters of the regression line fitted
+lr.summary()
+
+# Visualizing the regression line
+plt.scatter(X_train, y_train)
+plt.plot(X_train, 6.948 + 0.054*X_train, 'r')
+plt.show()
+
+# Residual analysis
+# Predicting y_value using traingn data of X
+y_train_pred = lr.predict(X_train_sm)
+
+# Creating residuals from the y_train data and predicted y_data
+res = (y_train - y_train_pred)
+
+# Plotting the histogram using the residual values
+fig = plt.figure()
+sns.distplot(res, bins = 15)
+plt.title('Error Terms', fontsize = 15)
+plt.xlabel('y_train - y_train_pred', fontsize = 15)
+plt.show()
+
+# Looking for any patterns in the residuals
+plt.scatter(X_train,res)
+plt.show()
+
+
+# Predictions on the Test data or Evaluating the model
+# Adding a constant to X_test
+X_test_sm = sm.add_constant(X_test)
+
+# Predicting the y values corresponding to X_test_sm
+y_test_pred = lr.predict(X_test_sm)
+
+# Printing the first 15 predicted values
+y_test_pred
+
+# Checking the R-squared value
+r_squared = r2_score(y_test, y_test_pred)
+r_squared
+
+'''
+Since the R² value on test data is within 5% of the R² value 
+on training data, we can conclude that the model is pretty stable. 
+Which means, what the model has learned on the training set can 
+generalize on the unseen test set.
+'''
+
+# Visualize the line on the test set
+plt.scatter(X_test, y_test)
+plt.plot(X_test, y_test_pred, 'r')
+plt.show()
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the diabetes dataset
+diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
+
+# Use only one feature
+diabetes_X = diabetes_X[:, np.newaxis, 2]
+
+# Split the data into training/testing sets
+diabetes_X_train = diabetes_X[:-20]
+diabetes_X_test = diabetes_X[-20:]
+
+# Split the targets into training/testing sets
+diabetes_y_train = diabetes_y[:-20]
+diabetes_y_test = diabetes_y[-20:]
+
+# Create linear regression object
+regr = linear_model.LinearRegression()
+
+# Train the model using the training sets
+regr.fit(diabetes_X_train, diabetes_y_train)
+
+# Make predictions using the testing set
+diabetes_y_pred = regr.predict(diabetes_X_test)
+
+# The coefficients
+print('Coefficients: \n', regr.coef_)
+# The mean squared error
+print('Mean squared error: %.2f'
+      % mean_squared_error(diabetes_y_test, diabetes_y_pred))
+# The coefficient of determination: 1 is perfect prediction
+print('Coefficient of determination: %.2f'
+      % r2_score(diabetes_y_test, diabetes_y_pred))
+
+# Plot outputs
+plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
+plt.plot(diabetes_X_test, diabetes_y_pred, color='blue', linewidth=3)
+
