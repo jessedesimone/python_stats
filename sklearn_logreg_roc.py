33a7a43982202c253e33a71fcf85b462bffbd897
jessedesimone <desimone.neuro@gmail.com>
2021-10-04 15:18:50 -0400

    initial commit

--- sklearn_logreg_roc.py
+++ sklearn_logreg_roc.py
@@ -2,6 +2,8 @@
 """
 Copyright (C) 2021 Jesse DeSimone, Ph.D.
 
+Some source code: https://www.kaggle.com/yogidsba/diabetes-prediction-eda-model
+
 Change Log
 =============
 0.0.1 (2021-10-04)
@@ -13,14 +15,15 @@ Initial commit
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 # Read data
 data = pd.read_csv("/Users/jessedesimone/DeSimone_Github/python_stats/Datasets/diabetes.csv")
 data.info()
 
 # Define predictors and outcome
-X = data.iloc[:,0:-1].values
-y = data.iloc[:,-1].values
+X = data.drop('Outcome', axis=1)
+y = data['Outcome']
 
 '''
 Here, could use some preliminary correlation analysis to determine
@@ -74,37 +77,90 @@ list_col = data.select_dtypes(include='number').columns.to_list()
 for i in range(len(list_col)):
     dist_box(data[list_col[i]])
 
-# Build logistic regression model
-
 # Split the data
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 4)
 X_train.shape, y_train.shape
 X_test.shape, y_test.shape
 
-# Use standard scaler to normalize the data across predictors
+# Create and fit StandardScaler instance
 from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()    #create StandardScaler instance
-X_train = sc_X.fit_transform(X_train)     #fit StandardScaler
-X_test = sc_X.transform(X_test)
+scaler = StandardScaler()
+X_scaler = scaler.fit(X_train)
 
+# Scaling data
+X_train_scaled = X_scaler.transform(X_train)
+X_test_scaled = X_scaler.transform(X_test)
+
+X_train_scaled_df = pd.DataFrame(X_train_scaled,columns=X_train.columns)
+X_test_scaled_df = pd.DataFrame(X_test_scaled,columns=X_test.columns)
+
+X_train_scaled_df.index=np.arange(len(X_train_scaled_df))
+X_test_scaled_df.index=np.arange(len(X_test_scaled_df))
+y_train.index=np.arange(len(y_train))
+y_test.index=np.arange(len(y_test))
+
+# Function to create confusion matrix
+from sklearn.metrics import confusion_matrix,ConfusionMatrixDisplay,plot_confusion_matrix
+def make_confusion_matrix(y_actual, y_predict, title):
+    fig, ax = plt.subplots(1, 1)
+    cm = confusion_matrix(y_actual, y_predict, labels=[0, 1])
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm,
+                                  display_labels=["No", "Yes"])
+    disp.plot(cmap='Reds', colorbar=True, ax=ax)
+    ax.set_title(title)
+    plt.tick_params(axis=u'both', which=u'both', length=0)
+    plt.grid(b=None, axis='both', which='both', visible=False)
+    plt.show()
+
+from sklearn.metrics import  classification_report, accuracy_score, precision_score, recall_score,f1_score
+# Function to calculate different metric scores of the model - Accuracy, Recall and Precision
+def get_metrics_score(model, X_train_df, X_test_df, y_train_pass, y_test_pass, flag=True):
+    '''
+    model : classifier to predict values of X
+    '''
+    # defining an empty list to store train and test results
+    score_list = []
+    pred_train = model.predict(X_train_df)
+    pred_test = model.predict(X_test_df)
+    pred_train = np.round(pred_train)
+    pred_test = np.round(pred_test)
+    train_acc = accuracy_score(y_train_pass, pred_train)
+    test_acc = accuracy_score(y_test_pass, pred_test)
+    train_recall = recall_score(y_train_pass, pred_train)
+    test_recall = recall_score(y_test_pass, pred_test)
+    train_precision = precision_score(y_train_pass, pred_train)
+    test_precision = precision_score(y_test_pass, pred_test)
+    train_f1 = f1_score(y_train_pass, pred_train)
+    test_f1 = f1_score(y_test_pass, pred_test)
+    score_list.extend(
+        (train_acc, test_acc, train_recall, test_recall, train_precision, test_precision, train_f1, test_f1))
+
+    if flag == True:
+        print("\x1b[0;30;47m \033[1mMODEL PERFORMANCE\x1b[0m")
+        print("\x1b[0;30;47m \033[1mAccuracy   : Train:\x1b[0m", round(train_acc, 3),
+              "\x1b[0;30;47m \033[1mTest:\x1b[0m ", round(test_acc, 3))
+        print("\x1b[0;30;47m \033[1mRecall     : Train:\x1b[0m", round(train_recall, 3),
+              "\x1b[0;30;47m \033[1mTest:\x1b[0m", round(test_recall, 3))
+
+        print("\x1b[0;30;47m \033[1mPrecision  : Train:\x1b[0m", round(train_precision, 3),
+              "\x1b[0;30;47m \033[1mTest:\x1b[0m ", round(test_precision, 3))
+        print("\x1b[0;30;47m \033[1mF1         : Train:\x1b[0m", round(train_f1, 3),
+              "\x1b[0;30;47m \033[1mTest:\x1b[0m", round(test_f1, 3))
+        make_confusion_matrix(y_train_pass, pred_train, "Confusion Matrix for Train")
+        make_confusion_matrix(y_test_pass, pred_test, "Confusion Matrix for Test")
+    return score_list  # returning the list with train and test scores
+
+
+# Build logistic regression model
 from sklearn.linear_model import LogisticRegression
 lr_mod = LogisticRegression()
-lr_mod.fit(X_train, y_train)
-y_pred = lr_mod.predict(X_test)
-y_pred_logistic = lr_mod.decision_function(X_test)  #use this option when plotting ROC
-
-from sklearn import metrics
-cm = metrics.confusion_matrix(y_test, y_pred)
-print(cm)
-
-# Accuracy score
-from sklearn.metrics import accuracy_score
-accuracy_score(y_test, y_pred)
+lr_mod.fit(X_train_scaled_df, y_train)
+score_list_dt=get_metrics_score(lr_mod,X_train_scaled_df,X_test_scaled_df,y_train,y_test)
 
 # Cross validation
 from sklearn.model_selection import cross_val_score
-scores = cross_val_score(lr_mod, X_train, y_train, cv=5)
+scores = cross_val_score(lr_mod, X_train_scaled_df, y_train, cv=5)
 print('Cross-Validation Accuracy Scores', scores)
 scores = pd.Series(scores)
 min_cv = scores.min()
@@ -114,7 +170,8 @@ print('Mean cross-validation score: {:.2f}'.format(mean_cv))
 
 # Plot receiver operating characteristic (ROC) Area-under-the-curve (AUC)
 from sklearn.metrics import roc_curve, auc
-logistic_fpr, logistic_tpr, threshold = roc_curve(y_test, y_pred_logistic)
+y_pred = lr_mod.decision_function(X_test)
+logistic_fpr, logistic_tpr, threshold = roc_curve(y_test, y_pred)
 auc_logistic = auc(logistic_fpr, logistic_tpr)
 
 plt.figure(figsize=(5, 5), dpi=100)
