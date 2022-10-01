18268cd33e4cf4f132d928cdf6e8e7f6525e6452
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2022-09-30 23:15:09 -0400

    9.30

--- sm_lin_regress.py
+++ sm_lin_regress.py
@@ -1,103 +1,54 @@
 #!/usr/local/bin/python3.9
 
-'''
-Code derived from Kaushik Katari (https://towardsdatascience.com/simple-linear-regression-model-using-python-machine-learning-eab7924d18b4)
-'''
-
+# Import packages
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split
 import statsmodels.api as sm
-from sklearn.metrics import r2_score
+import numpy as np
 
 # Read the given CSV file, and view some sample records
-inpath = '<path/to/file>'
-df = pd.read_csv(inpath + 'Company_data.csv')
+inpath = '/Users/jessedesimone/Desktop/'
+df = pd.read_csv(inpath + 'data.csv')
 df.info()
 df.describe()
 
+#define variables
+X='month'
+y='savings'
+
+X_var = df[X]
+y_var = df[y]
+
+# Adding a constant to get an intercept
+X_var_sm = sm.add_constant(X_var)
+
 # Visualize the data wrt to Sales
-sns.pairplot(df, x_vars=['TV', 'Radio','Newspaper'],
-             y_vars='Sales', size=4, aspect=1, kind='scatter')
+sns.pairplot(df, x_vars=X, y_vars=y, height=4, aspect=1, kind='scatter')
 plt.show()
 
 # Visualize the data using heatmap
 sns.heatmap(df.corr(), cmap="YlGnBu", annot = True)
 plt.show()
 
-# Create X and y
-X = df['TV']
-y = df['Sales']
-
-# # Splitting the varaibles as training and testing
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    train_size = 0.7,
-                                                    test_size = 0.3,
-                                                    random_state = 100)
-X_train
-y_train
-
-# Building and training the model
-# Adding a constant to get an intercept
-X_train_sm = sm.add_constant(X_train)
-
 # Fitting the resgression line using 'OLS'
-lr = sm.OLS(y_train, X_train_sm).fit()
+mod = sm.OLS(y_var, X_var_sm).fit()
 
-# Printing the parameters
-lr.params
+# Summary of the regression output
+mod.summary()
 
-# Performing a summary to list out all the different parameters of the regression line fitted
-lr.summary()
-
-# Visualizing the regression line
-plt.scatter(X_train, y_train)
-plt.plot(X_train, 6.948 + 0.054*X_train, 'r')
-plt.show()
+# Model parameters
+params=mod.params
+print(params)
 
 # Residual analysis
 # Predicting y_value using traingn data of X
-y_train_pred = lr.predict(X_train_sm)
+y_pred = mod.predict(X_var_sm)
 
 # Creating residuals from the y_train data and predicted y_data
-res = (y_train - y_train_pred)
+res = (y_var - y_pred)
 
-# Plotting the histogram using the residual values
-fig = plt.figure()
-sns.distplot(res, bins = 15)
-plt.title('Error Terms', fontsize = 15)
-plt.xlabel('y_train - y_train_pred', fontsize = 15)
-plt.show()
-
-# Looking for any patterns in the residuals
-plt.scatter(X_train,res)
-plt.show()
-
-# Predictions on the Test data or Evaluating the model
-# Adding a constant to X_test
-X_test_sm = sm.add_constant(X_test)
-
-# Predicting the y values corresponding to X_test_sm
-y_test_pred = lr.predict(X_test_sm)
-
-# Printing the first 15 predicted values
-y_test_pred
-
-# Checking the R-squared value
-r_squared = r2_score(y_test, y_test_pred)
-r_squared
-
-'''
-Since the R² value on test data is within 5% of the R² value 
-on training data, we can conclude that the model is pretty stable. 
-Which means, what the model has learned on the training set can 
-generalize on the unseen test set.
-'''
-
-# Visualize the line on the test set
-plt.scatter(X_test, y_test)
-plt.plot(X_test, y_test_pred, 'r')
-plt.show()
-
-
+# Predict future value
+X_new=np.array(1,12)    # "1" refers to the intercept term
+prediction=mod.get_prediction(X_new)
+prediction.summary_frame()
