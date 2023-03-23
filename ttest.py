1b425cf0e9ce5597a8f6e719acff67a2b9df4cd6
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-03-23 15:09:16 -0400

    230323

--- ttest.py
+++ ttest.py
@@ -2,16 +2,20 @@ import pandas as pd
 from scipy import stats
 from scipy.stats import ttest_ind
 
-df=pd.read_csv('/Users/jessedesimone/Documents/postdoc_uf/analysis/ab4240_imaging/fmriconnmap/df_pred.csv')
+#read data
+df=pd.read_csv('/path/to/csv')
 
-df_check=df.filter(regex='grp|cdr')
-df_check=df_check.drop(['cdr_lab'], axis=1)
+#subset data
+df_check=df.filter(regex='grp|apoe_lab')
+#df_check=df_check.drop(['ab42_40_label'], axis=1)
+#df_check=df_check.drop(['cdr_lab', 'moca_lab', 'mmse_lab'], axis=1)
 
 #describe data
-
+df_check.groupby(['grp']).describe()
 
 #independent samples t-test
+test='suvr'
 group1 = df_check[df_check['grp']=='high_risk']
 group2 = df_check[df_check['grp']=='low_risk']
-t_stat, p_value = ttest_ind(group1['cdr'], group2['cdr'])
+t_stat, p_value = ttest_ind(group1[test], group2[test])
 print("T-statistic value: ", t_stat); print("P-Value: ", p_value)
