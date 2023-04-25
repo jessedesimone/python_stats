bf84b27fab24f6cbb28fca98826209decb6fcb51
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-04-25 16:03:10 -0400

    230425

--- FDR_BH.py
+++ FDR_BH.py
@@ -1,50 +1,10 @@
 #!/usr/local/bin/python3.9
-"""
-Copyright (C) 2021 Jesse DeSimone, Ph.D.
 
-Change Log
-=============
-0.0.1 (2021-04-09)
--------------
-Initial commit
+'''
+run the number_req() function and manually input all uncorrected p-values
+then run p_adj_bh() function to perform FDR corrected for uncorrected p-values
+'''
 
-Usage:
-    user input {int} number of uncorrected p-values to be entered
-    user input {float} uncorrected pvalues
-
-Output:
-    dataframe with uncorrected and correcte values
-    FDR_out.csv
-
-
-Other options:
-
-# Input list of uncorrected P-values
-pv = input('\nEnter p-valus uncorrected: ')     #input string of uncorrected p-values
-pvals = [float(s) for s in pv.split(',')]       #convert string to list of floats
-
-# Convert DataFrame array to list
-infile = '<path/to/file>'
-df=pd.read_excel(infile)
-pvals=df['pvals'].tolist()
-
-# Use statsmodels package
-import statsmodels.stats.multitest as multi
-multi.multipletests(pvals, alpha=0.05, method='fdr_bh', is_sorted=False, returnsorted=False)
-multitest_methods_names = {'b': 'Bonferroni',
-                           's': 'Sidak',
-                           'h': 'Holm',
-                           'hs': 'Holm-Sidak',
-                           'sh': 'Simes-Hochberg',
-                           'ho': 'Hommel',
-                           'fdr_bh': 'FDR Benjamini-Hochberg',
-                           'fdr_by': 'FDR Benjamini-Yekutieli',
-                           'fdr_tsbh': 'FDR 2-stage Benjamini-Hochberg',
-                           'fdr_tsbky': 'FDR 2-stage Benjamini-Krieger-Yekutieli',
-                           'fdr_gbs': 'FDR adaptive Gavrilov-Benjamini-Sarkar'
-                           }
-
-"""
 # PACKAGES
 import numpy as np
 import pandas as pd
