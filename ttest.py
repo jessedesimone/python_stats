c31642aeb2509925c4b4ee9b21369ba74538d8d7
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-04-25 12:13:24 -0400

    230425

--- ttest.py
+++ ttest.py
@@ -26,12 +26,24 @@ print("T-statistic value: ", t_stat); print("P-Value: ", p_value)
 perform ttest on multiple variables
 update grouping variable
 '''
+#create lists for final dataframe
+col_list=df_check.columns.to_list()
+col_list.remove('grp')
+t_list = []
+p_unc_list = []
+
 loop_cols = df_check.loc[:, df_check.columns!='<grouping var>']
 col_names = [col for col in loop_cols]
 for col in col_names:
     group1 = df_check[df_check['<grouping var>']=='<grouping sub var>']
     group2 = df_check[df_check['<grouping var>']=='<grouping sub var>']
     t_stat, p_value = ttest_ind(group1[col], group2[col])
+    t_list.append(t_stat)
+    p_unc_list.append(p_value)
     print("")
     print("*** Variable: ", col)
     print("T-statistic value: ", t_stat); print("P-Value: ", p_value)
+
+d = {'variable':col_list, 't_stat':t_list, 'p_unc':p_unc_list}
+df_f = pd.DataFrame(d)
+print(df_f)
