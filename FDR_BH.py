883945d4231382f3aa644d6580ad2268d9e18f88
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-04-26 12:39:08 -0400

    230426

--- FDR_BH.py
+++ FDR_BH.py
@@ -1,12 +1,24 @@
 #!/usr/local/bin/python3.9
 
 '''
+Option 1
 run the number_req() function and manually input all uncorrected p-values
 then run p_adj_bh() function to perform FDR corrected for uncorrected p-values
+
+Option 2
+create csv file with 2 columns
+column 1 is a list of variable names with column name 'variables'
+column 2 is the associated uncorrected p-value for each variable with column name 'p_unc'
+run function
+
 '''
 # PACKAGES
 import numpy as np
 import pandas as pd
+import statsmodels as sm
+
+
+#------------------------- option 1 -------------------------
 
 # Define Runner function
 list_of_p = []
@@ -39,3 +51,20 @@ d = {'Uncorrected':list_of_p, 'FDR':newvals}
 df = pd.DataFrame(d)
 print(df)
 df.to_csv('/Users/jessedesimone/Desktop/FDR_out.csv')
+
+#------------------------- option 2 -------------------------
+df = pd.read_csv('/path/to/pvals.csv')
+var_list=df['variable'].tolist(); var_list
+p_unc_list=df['p_unc'].tolist(); p_unc_list
+
+a=sm.stats.multitest.fdrcorrection(p_unc_list, 
+                                          alpha=0.05, 
+                                          method='indep', 
+                                          is_sorted=False)
+fdr_list=a[1].tolist()
+
+'''create dataframe for statistics and uncorrected pvalues'''
+d = {'variable':var_list, 'p_unc':p_unc_list, 'fdr':fdr_list}
+df_f = pd.DataFrame(d)
+print(df_f)
+df_f.to_csv('/path/to/fdr_out.csv')
--- permutation_test.py
+++ permutation_test.py
@@ -4,19 +4,61 @@
 import pandas as pd
 
 #read data
-df = pd.read_csv('/path/to/csv')
+df = pd.read_csv('/path/to/perm_test.csv')
+df_c=df     #make a copy of df
 
 #subset the dataframe
-df_check=df.filter(regex='var1|var2|var n')
+#df_c=df.filter(regex='var1|var2|var n')
 
 #describe data
-df_check.groupby(['<grouping var>']).describe()
+d=df_c.groupby(['<grouping var>']).describe()
+print(d)
+d.to_csv('/path/to/out.csv')
+
+#define statistic for permutation test
+def statistic(x, y, axis):
+    return np.mean(x, axis=axis) - np.mean(y, axis=axis)
 
 #permutation test for single variable
+'''define variables'''
+test='ab42_40'
+x = df_c[df_c['grp']=='<sub group e.g. control>']
+y = df_c[df_c['grp']=='<sub group eg. treatment>']
 
+'''run permutation test for single variable'''
+res = permutation_test((x[test], y[test]), statistic, permutation_type='independent', 
+                       n_resamples=10000, alternative='two-sided', random_state=None)
+print('test statistic=',res.statistic,'     pvalue=',res.pvalue)
 
 #permutation tests for multiple variables
+'''create lists for final dataframe'''
+col_list=df_c.columns.to_list()
+col_list.remove('grp')
+t_list = []
+p_unc_list = []
 
-
+'''permutation test loop'''
+loop_cols = df_c.loc[:, df_c.columns!='<grouping var>']
+col_names = [col for col in loop_cols]
+for col in col_names:
+    print('*** running permutation test for ',col)
+    x = df_c[df_c['grp']=='<sub group e.g. control>']
+    y = df_c[df_c['grp']=='<sub group eg. treatment>']
+    res = permutation_test((x[col], y[col]), statistic, vectorized=True, permutation_type='independent', 
+                       n_resamples=10000, alternative='two-sided', random_state=None)
+    t_list.append(res.statistic)
+    p_unc_list.append(res.pvalue)
 
 #FDR correction for multivariate permutation tests
+import statsmodels
+a=statsmodels.stats.multitest.fdrcorrection(p_unc_list, 
+                                          alpha=0.05, 
+                                          method='indep', 
+                                          is_sorted=False)
+fdr_list=a[1].tolist()
+
+#create a final dataframe
+'''create dataframe for statistics and uncorrected pvalues'''
+d = {'variable':col_list, 't_stat':t_list, 'p_unc':p_unc_list, 'fdr':fdr_list}
+df_f = pd.DataFrame(d)
+print(df_f)
