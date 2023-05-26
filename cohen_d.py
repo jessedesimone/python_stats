00ffcad021b69e3069bd7d8dff37b42e36be2559
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-05-26 11:35:01 -0400

    230526

--- cohen_d.py
+++ cohen_d.py
@@ -14,10 +14,10 @@ import pandas as pd
 import math
 
 #configure directories and files
-IN_DIR='/Users/jessedesimone/Desktop'
-OUT_DIR='/Users/jessedesimone/Desktop'
-infile='test_data.csv'
-outfile='test_data_cohen.csv'
+IN_DIR='/path/to/infile/directory'
+OUT_DIR='/path/to/outfile/directory'
+infile='<input file>.csv'
+outfile='<output file>.csv'
 fname=os.path.join(IN_DIR,infile)
 oname=os.path.join(OUT_DIR,outfile)
 
@@ -25,11 +25,11 @@ oname=os.path.join(OUT_DIR,outfile)
 df = pd.read_csv(fname)
 
 #define group names
-grp_var='grp'
-grp1='A-'
-grp2='A+'
+grp_var=''
+grp1=''
+grp2=''
 
-#define functions
+#define cohen function
 def cohen(data, grouping_var, group1, group2, column_name):
     m1=data.loc[df[grouping_var] == group1, column_name].mean()
     m2=data.loc[df[grouping_var] == group2, column_name].mean()
@@ -41,9 +41,6 @@ def cohen(data, grouping_var, group1, group2, column_name):
     s_pool=math.sqrt(((n1-1)*s1*s1+(n2-1)*s2*s2)/(n1+n2-2))
     cohen=m_diff/s_pool
     return m1,s1,n1,m2,s2,n2,m_diff,s_pool,cohen
-
-#test for cohen function
-#cohen(df, grp_var, grp1, grp2, 'roi1')
 
 #create lists for final dataframe
 var_list=df.columns.tolist(); var_list.remove(grp_var)      #variable list
@@ -57,7 +54,7 @@ m_diff_list=[]
 s_pool_list=[]
 cohen_list=[]
 
-#run cohen function for all columns
+#run cohen function for all columns in dataframe except grouping variable
 df_c=df         #create dataframe copy
 loop_cols = df_c.loc[:, df_c.columns!=grp_var]
 col_names = [col for col in loop_cols]
@@ -73,7 +70,6 @@ for col in col_names:
     m_diff=c[6]
     s_pool=c[7]
     coh=c[8]
-    '''append each output to list'''
     m1_list.append(m1)
     s1_list.append(s1)
     n1_list.append(n1)
@@ -84,7 +80,7 @@ for col in col_names:
     s_pool_list.append(s_pool)
     cohen_list.append(coh)
 
-'''create dataframe for statistics and uncorrected pvalues'''
+#create new dataframe with values for all colnames and output to csv file
 d = {'variable':var_list, 'mean_1':m1_list, 'mean_2':m2_list, 'mean_diff':m_diff_list,
      'sd_1':s1_list, 'sd_2':s2_list, 'pooled_sd':s_pool_list, 'Cohen d':cohen_list}
 df_f = pd.DataFrame(d)
