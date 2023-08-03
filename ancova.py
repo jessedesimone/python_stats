6f3f8d4530b50ac5f5955b0a5d5893c2f4360427
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-08-03 12:01:43 -0400

    230803

--- ancova.py
+++ ancova.py
@@ -1,1 +1,19 @@
 #!/usr/local/bin/python3.9
+
+#import packages
+import os
+import pandas as pd
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+
+#read data
+df = pd.read_csv('/Users/jessedesimone/Desktop/test_ancova.csv')
+
+#run ancova model
+model = ols('Amygdala_R_FW ~ C(grp_id) + age + C(sex) + educ + C(apoe)', data=df).fit()
+model_sum = sm.stats.anova_lm(model, typ=3)
+print(model_sum)
+
+#tukey comparisons
+mc = pairwise_tukeyhsd(df['Amygdala_R_FW'],df['grp_id']); mc_results = mc.summary(); print(mc_results)
